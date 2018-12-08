7643718dc4d80d32a63123dc99ab7d0fde7a738b
Michael Gustafsson <michael.gustavsson@skaftoskorpan.se>
2018-12-08 20:39:56 +0100

    Added Mod11 source code

---
+++ Mod11/AuthorizeDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthorizeDemo.Startup))]
+namespace AuthorizeDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
