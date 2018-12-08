e961e87605440dada6ce89bf48a2589fb5a6bcd9
Michael Gustafsson <michael.gustavsson@skaftoskorpan.se>
2018-12-08 20:43:00 +0100

    Added Mod12 source code

---
+++ Mod12/CourseTracker/Controllers/CoursesController.cs
@@ -0,0 +1,116 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using CourseTrack.Models;
+
+namespace CourseTrack.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private CourseTrackerContext db = new CourseTrackerContext();
+
+        public ActionResult Index()
+        {
+            var courses = db.Courses.Include(c => c.Instructor);
+            return View(courses.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.InstructorId = new SelectList(db.Instructors, "InstructorId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CourseId,CourseNumber,Title,Description,Days,ReleaseDate,InstructorId")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorId = new SelectList(db.Instructors, "InstructorId", "Name", course.InstructorId);
+            return View(course);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstructorId = new SelectList(db.Instructors, "InstructorId", "Name", course.InstructorId);
+            return View(course);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CourseId,CourseNumber,Title,Description,Days,ReleaseDate,InstructorId")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(course).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorId = new SelectList(db.Instructors, "InstructorId", "Name", course.InstructorId);
+            return View(course);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Course course = db.Courses.Find(id);
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Mod12/CourseTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseTracker.Startup))]
+namespace CourseTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
