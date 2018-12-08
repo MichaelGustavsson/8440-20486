b96d5c425904e16f64b5268abe9867db14449ced
Michael Gustafsson <michael.gustavsson@skaftoskorpan.se>
2018-12-08 20:28:51 +0100

    Added Mod09 source code

---
+++ Mod09/CourseTrack/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseTrack.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        [StringLength(512)]
+
+        [DisplayName("Comment")]
+        public string Body { get; set; }
+        public int CourseId { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
---
+++ Mod09/CourseTrack/Models/CourseTrackerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CourseTrack.Models
+{
+    public class CourseTrackerContext: DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
