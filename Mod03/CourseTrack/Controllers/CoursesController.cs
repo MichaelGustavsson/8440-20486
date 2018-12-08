55c1bab76b106b92078e1a3d28c69070b70d872a
Michael Gustafsson <michael.gustavsson@skaftoskorpan.se>
2018-12-08 19:53:20 +0100

    Added Mod03 demo code

---
+++ Mod03/CourseTrack/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using CourseTrack.Models;
+using CourseTrack.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseTrack.Controllers
+{
+    public class CoursesController : Controller
+    {
+        //readonly CourseTrackerContext _context = new CourseTrackerContext();
+        ICourseRepository _repo = new CourseRepository();
+
+        public ActionResult Index()
+        {
+            // var courses = _context.Courses.ToList();
+            var courses = _repo.ListCourses();
+
+            return View(courses);
+        }
+    }
+}
---
+++ Mod03/CourseTrack/Models/CourseTrackerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CourseTrack.Models
+{
+    public class CourseTrackerContext: DbContext
+    {
+        public CourseTrackerContext():base("DefaultConnection")
+        {
+            Database.CommandTimeout = 180;
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+
+    }
+}
