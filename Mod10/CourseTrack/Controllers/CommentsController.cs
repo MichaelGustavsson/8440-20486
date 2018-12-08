36e9e04a92a78c1fdf4a3d839c2419d5cf3e3bd3
Michael Gustafsson <michael.gustavsson@skaftoskorpan.se>
2018-12-08 20:33:56 +0100

    Added Mod10 source code

---
+++ Mod10/CourseTrack/Controllers/CommentsController.cs
@@ -0,0 +1,39 @@
+﻿using CourseTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseTrack.Controllers
+{
+    public class CommentsController : Controller
+    {
+        CourseTrackerContext _context = new CourseTrackerContext();
+
+        public ActionResult _GetCommentsForCourse(int courseId)
+        {
+            ViewBag.CourseId = courseId;
+            var comments = _context.Comments.Where(c => c.CourseId == courseId).ToList();
+
+            return PartialView("_GetCommentsForCourse", comments);
+        }
+
+        [ChildActionOnly]
+        public ActionResult _GetCommentForm(int courseId)
+        {
+            Comment comment = new Comment() { CourseId = courseId };
+            return PartialView("_CommentForm", comment);
+        }
+
+        public ActionResult _SaveComment(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            _context.SaveChanges();
+
+            var comments = _context.Comments.Where(c => c.CourseId == comment.CourseId).ToList();
+            ViewBag.CourseId = comment.CourseId;
+            return PartialView("_GetCommentsForCourse", comments);
+        }
+    }
+}
---
+++ Mod10/CourseTrack/Scripts/courses.js
@@ -0,0 +1,4 @@
+﻿
+$.when(
+    $("#courseCalender").datepicker()
+);
