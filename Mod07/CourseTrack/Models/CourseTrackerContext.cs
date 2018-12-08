d2f02ce4fa6065d037fbcad3da7fc0c5cbe56f18
Michael Gustafsson <michael.gustavsson@skaftoskorpan.se>
2018-12-08 20:15:35 +0100

    Added Mod07 source code

---
+++ Mod07/CourseTrack/Models/CourseTrackerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CourseTrack.Models
+{
+    public class CourseTrackerContext: DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+
+    }
+}
